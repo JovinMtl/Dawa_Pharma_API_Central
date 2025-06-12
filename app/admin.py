2c4983124933523ba4df1b9690250fbd2663a7eb
JovinMtl <nsanzumukizath@gmail.com>
2025-06-12 20:47:23 +0200

    Alter field date_per on medcollection

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User, Pharma, MedCollection
+from .models import Pharma, MedCollection
 
 
-admin.site.register(User)
 admin.site.register(Pharma)
 admin.site.register(MedCollection)
--- app/models.py
+++ app/models.py
@@ -2,11 +2,18 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+def first_user():
+    user = User.objects.first()
+    return user
+
+
 # Create your models here.
 
 class Pharma(models.Model):
-    name_pharma = models.CharField(max_length=35, default="Pharma")
+    name_pharma = models.CharField(max_length=35, default="Pharma", unique=True)
     code_pharma = models.CharField(max_length=8, default="PH000")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     loc_street = models.CharField(max_length=15, default="13")
     loc_quarter = models.CharField(max_length=15, default="Kamenge")
     loc_commune = models.CharField(max_length=15, default="Ntahangwa")
@@ -16,18 +23,16 @@ class Pharma(models.Model):
     joined_on = models.DateTimeField(default=timezone.now)
     last_connected = models.DateTimeField(default=timezone.now)
 
+    def __str__(self) -> str:
+        return f"{self.name_pharma}"
     class Meta:
         verbose_name = "Pharmacie"
-
-
-class User(User):
-    pharma = models.ForeignKey(Pharma, on_delete=models.CASCADE)
 
 
 class MedCollection(models.Model):
     nom_med = models.CharField(max_length=75, default="med")
     qte = models.IntegerField(default=0)
     price = models.IntegerField(default=0)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Pharma, on_delete=models.CASCADE)
     score = models.IntegerField(default=0)
-    date_per = models.DateField(default=timezone.now)
+    date_per = models.TextField()
