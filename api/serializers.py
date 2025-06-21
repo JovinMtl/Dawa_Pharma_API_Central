f61b28c9db23a07d7eed285f6a08f46ed640ec0c
JovinMtl <nsanzumukizath@gmail.com>
2025-06-21 06:26:26 +0200

    serializer for Pharma

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from app.models import MedCollection
+from app.models import MedCollection, Pharma
 
 
 class MedCollectionSeria(serializers.ModelSerializer):
     class Meta:
         model = MedCollection
+        fields = '__all__'
+
+
+class PharmaSeria(serializers.ModelSerializer):
+    class Meta:
+        model = Pharma
         fields = '__all__'
